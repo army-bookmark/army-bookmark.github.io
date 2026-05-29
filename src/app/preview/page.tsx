9a3f98fb4f50fe401ee896c3444f202ea9655434
orange-table <workingwithristi@gmail.com>
2026-05-30 00:13:47 +0700

    Code Clean

--- src/app/preview/page.tsx
+++
@@ -1,7 +0,0 @@
-import { getSheetData } from '@/lib/sheets'
-import { PreviewClient } from '@/components/PreviewClient'
-
-export default async function PreviewPage() {
-  const cards = await getSheetData()
-  return <PreviewClient cards={cards} />
-}
