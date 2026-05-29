a18a5cd9afa74afc795ef26f19cb4b964772d569
orange-table <workingwithristi@gmail.com>
2026-05-30 00:22:15 +0700

    Fix Code

--- next.config.ts
+++ next.config.ts
@@ -1,6 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
+ output: 'export',              // for static export
   trailingSlash: true,           // generates index.html per route (required for GitHub Pages)
   basePath: process.env.NEXT_PUBLIC_BASE_PATH ?? '',
   images: { unoptimized: true },
